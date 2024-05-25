c922a77e8877bfb5b97b3f01866ba1c9af1b501e
AnkitBali <18bcs015@smvdu.ac.in>
2024-05-25 13:43:17 +0530

    test1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Gallery from "./ui/components/Gallery";
 import ZigZagPosts from "./ui/components/ZigZagPosts";
 import Navbar from "./ui/components/Navbar";
 import RecentShows from "./ui/components/RecentShows";
-import EllipseComponent from "./ui/components/EllipseComponent";
+// import EllipseComponent from "./ui/components/EllipseComponent";
 
 
 
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
       <Gallery />
       <ZigZagPosts posts={posts} />
       <RecentShows />
-      <EllipseComponent />
+      {/* <EllipseComponent /> */}
 
     </ChakraProvider>
     
