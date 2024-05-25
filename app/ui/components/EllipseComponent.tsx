8d2fc9478612cc7785b43f423e161348631785e2
AnkitBali <18bcs015@smvdu.ac.in>
2024-05-25 13:18:43 +0530

    more updated on last component

--- app/ui/components/EllipseComponent.tsx
+++ app/ui/components/EllipseComponent.tsx
@@ -5,6 +5,7 @@ import { Box, Button, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import 'tailwindcss/tailwind.css';
 import CustomButton from './CustomButton';
+import '@fontsource/pacifico'; // Import Pacifico font
 
 const MotionBox = motion(Box);
 
@@ -27,6 +28,49 @@ const EllipseComponent: React.FC = () => {
         animate={{ scale: 1 }}
         transition={{ duration: 0.5 }}
       >
+        {/* New Text with Pacifico Font */}
+        <Text
+          className="absolute text-purple-500"
+          style={{
+            fontFamily: 'Pacifico',
+            fontSize: '30px',
+            transform: 'rotate(-20deg)',
+            top: '50px',
+            left: '150px',
+          }}
+        >
+          We are a team of
+        </Text>
+        {/* Curved Arrow SVG */}
+        <svg
+  className="absolute"
+  style={{ top: '-20px', left: '400px', transform: 'rotate(60deg)' }}
+  width="100"
+  height="100"
+  viewBox="0 0 100 100"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M10 90 C 40 10, 60 10, 90 90"
+    stroke="white"
+    strokeWidth="2"
+    fill="none"
+    markerEnd="url(#arrowhead)"
+  />
+  <defs>
+    <marker
+      id="arrowhead"
+      markerWidth="10"
+      markerHeight="7"
+      refX="10"
+      refY="3.5"
+      orient="auto"
+    >
+      <polygon points="0 0, 10 3.5, 0 7" fill="white" />
+    </marker>
+  </defs>
+</svg>
         <Text fontSize={"60px"} className="text-white text-center font-bold mb-2">20 + talented folks</Text>
         <Text width={"60%"} textAlign={"center"} fontSize={"20px"} className="text-white text-center mb-4">
           From passion driven dedication to impactful contributions, we do it all here. We are growing and we will be excited to hear from you!
